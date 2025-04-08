539c1b774a0a54c5123f72cda7b6a2c62fc92189
t-reinschmidt <timo-reinschmidt@hotmail.com>
2025-04-08 15:00:36 +0200

    starting migration to postgres

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Einsatz(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     datum = db.Column(db.String(20), nullable=False)
